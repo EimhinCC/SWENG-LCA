55de4ad72e1e77f98c0e4f8e1c64f5f24d0cac72
eimhincc <eimhincc@gmail.com>
2021-10-26 15:35:34 +0100

    more DAG tests

--- DAG_LCATest2.java
+++ DAG_LCATest2.java
@@ -10,7 +10,7 @@ class DAG_LCATest2 {
 			assertEquals("Check null tree", -1, tree.findLCA(1,1));        
 		}
 		@Test
-		final void twoRoots() {
+		final void twoParents() {
 			LCA tree = new LCA();
 			tree.root = new Node(1);
 			tree.root.addNode(2);
@@ -19,8 +19,20 @@ class DAG_LCATest2 {
 			tree.root.child[0].addNode(5);
 			tree.root.child[1].addNode(6);
 			tree.root.child[1].linkNode(tree.root.child[0].child[0]);
-			assertEquals("expect the first root", 3, tree.findLCA(4,6));        
-			assertEquals("expect the second root", 2, tree.findLCA(4,5));    
-		}
+			assertEquals("expect the first parent of node '4' ", 3, tree.findLCA(4,6));        
+			assertEquals("expect the second parent of node '4'", 2, tree.findLCA(4,5));    
+	}
 		
+		@Test
+		final void straightLine() {
+			LCA tree = new LCA();
+			tree.root = new Node(1);
+			tree.root.addNode(2);
+			tree.root.child[0].addNode(3);
+			tree.root.child[0].child[0].addNode(4);
+			tree.root.child[0].child[0].child[0].addNode(5);
+			tree.root.child[0].child[0].child[0].child[0].addNode(6);
+			assertEquals("expect the parent of node '4' ", 3, tree.findLCA(6,4));        
+			assertEquals("expect the parent of node '4'", 3, tree.findLCA(4,5));    
+		}
 }
